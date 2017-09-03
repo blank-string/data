547f15880a52ff52325afb2b35a4c030f6b12ae6
Luke Preston <lukejpreston@gmail.com>
2017-09-03 15:58:37 +0100

    updating all episodes

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ data = fs.readdirSync(__dirname)
     .filter(file => file.match(/^e\d+\.json$/g) !== null)
     .map(file => require(path.join(__dirname, file)))
     .sort((left, right) => {
-        if (left.time < right.time) return -1
-        if (left.time > right.time) return 1
+        if (left.time < right.time) return 1
+        if (left.time > right.time) return -1
         return 0
     })
 
